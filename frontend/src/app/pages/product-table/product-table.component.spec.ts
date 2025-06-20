e848bfbe683c072b446d455801014ddbca128dc4
Kristina <kristina.shatts@gmail.com>
2025-06-20 11:25:14 +0200

    add basic table setup for warehouse products

---
+++ frontend/src/app/pages/product-table/product-table.component.spec.ts
@@ -0,0 +1,32 @@
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+
+import { ProductTableComponent } from './product-table.component';
+
+describe('ProductTableComponent', () => {
+  let component: ProductTableComponent;
+  let fixture: ComponentFixture<ProductTableComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ProductTableComponent],
+      imports: [
+        MatPaginatorModule,
+        MatSortModule,
+        MatTableModule,
+      ]
+    });
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProductTableComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
